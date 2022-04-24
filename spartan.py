907775aebc385ccc3e72a61e35d59ded25c5a800
Jesus <jesus.alberto.vk@gmail.com>
2022-04-23 21:33:08 -0500

    Create new spartan class

---
+++ spartan.py
@@ -0,0 +1,40 @@
+from unicodedata import name
+
+
+class Spartan:
+
+    def __init__(self,  name, last_name, casco,   equipamiento,  voz,  sexo, antebrazo,   rodilleras,    stikers,    banderas,   color_visor):
+        self.__name = name
+        self.__last_name = last_name
+        self.__casco = casco
+        self.__equipamiento = equipamiento
+        self.__voz = voz
+        self.__sexo = sexo
+        self.__antebrazo = antebrazo
+        self.__rodilleras = rodilleras
+        self.__stikers = stikers
+        self.__banderas = banderas
+        self.__color_visor = color_visor
+    
+    def set_name(self,name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def get_antebrazo(self):
+        return self.__antebrazo
+
+    def get_full_name(self):
+        return self.__name + " " + self.__last_name
+
+will = Spartan(
+    "x","Diaz","cool",'f',
+    'No',"S",
+    "X","r",
+    "Karter","La de aczino","Purple")
+
+will.set_name('WILL')
+print(will.get_name())
+print(will.get_antebrazo())
+print(will.get_full_name())
